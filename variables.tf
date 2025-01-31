33cfffdf3c9898895bcf56e3d2fec372816aa35d
Mamatha1296 <mammumamatha1296@gmail.com>
2025-01-31 10:39:12 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "vpc-cidr-block" {
+    description = "The cidr block for the vpc"
+}
+
+variable "subnet-cidr" {
+    description = "The cidr block for the subnet"
+}
+
+variable "az" {
+    description = "The avaialability zone"  
+}
